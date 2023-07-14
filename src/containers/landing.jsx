9c4003d95e6936cd944c21424f807a83896978a0
Icen <105867625+icenspln@users.noreply.github.com>
2023-07-14 10:38:35 +0100

    finished main buttons decore

--- src/containers/landing.jsx
+++ src/containers/landing.jsx
@@ -28,13 +28,13 @@ export function Landing() {
           </p>
           <div className="flex gap-3 md:gap-8 items-center justify-start my-10">
             <a href="">
-              <button className="py-4 px-3  md:py-4 md:px-7 bg-yellow text-white rounded-md md:rounded-xl md:text-lg font-medium transition duration-300 hover:bg-orange">
+              <button className="py-4 px-3  md:py-4 md:px-7 bg-yellow text-white rounded-md md:rounded-xl md:text-lg font-medium transition duration-300 hover:bg-orange button-1">
                 Find out more
               </button>
             </a>
             <div className="flex items-center gap-3 md:gap-7">
-              <a href="" id="play">
-                <BsFillPlayFill className="text-white bg-orange hover:bg-yellow w-14 h-14 rounded-full p-4 transition duration-300" />
+              <a href="" id="play" className="button-2">
+                <BsFillPlayFill className="text-white bg-orange hover:bg-yellow w-14 h-14 rounded-full p-4 transition duration-300 " />
               </a>
               <label htmlFor="play" className="font-poppins text-lightGray">
                 Play Demo
