b6377e2496d3ecb77eca72095db029182f2e30b1
Icen <105867625+icenspln@users.noreply.github.com>
2023-07-15 15:42:08 +0100

    zindex bug

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 
 export function DestinationCard({ dest, price, duration }) {
   return (
-    <article className="dest-card flex rounded-[24px] w-[314px] overflow-hidden cursor-pointer transition duration-300 hover:scale-105">
-      <div className="pt-4 pb-8 px-6 bg-white mt-auto w-full font-poppins  text-lightGray">
+    <article className="dest-card flex rounded-[24px] w-[314px]  cursor-pointer ">
+      <div className="pt-4 pb-8 px-6 bg-white mt-auto w-full font-poppins  text-lightGray rounded-b-[24px]">
         <div className="flex justify-between gap-3 items-center">
           <p>{dest}</p>
           <p>${price}</p>
